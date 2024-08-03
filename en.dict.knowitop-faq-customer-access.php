7d43937110d20b08a647e9a45b43d73de50ad020
Vladimir Kunin <v.b.kunin@gmail.com>
2024-08-03 15:29:02 +0300

    first commit

---
+++ en.dict.knowitop-faq-customer-access.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2018 Vladimir Kunin https://knowitop.ru
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:FAQ/Attribute:status' => 'Status',
+	'Class:FAQ/Attribute:status+' => 'Published FAQs are displayed on the customer portal',
+	'Class:FAQ/Attribute:status/Value:draft' => 'draft',
+	'Class:FAQ/Attribute:status/Value:draft+' => '',
+	'Class:FAQ/Attribute:status/Value:published' => 'published',
+	'Class:FAQ/Attribute:status/Value:published+' => '',
+	'Class:FAQ/Attribute:status/Value:obsolete' => 'obsolete',
+	'Class:FAQ/Attribute:status/Value:obsolete+' => '',
+	'Class:FAQ/Attribute:customers_list' => 'Customers',
+	'Class:FAQ/Attribute:customers_list+' => 'This FAQ is available on the portal for persons of selected organizations',
+));
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:lnkFAQToCustomer' => 'Link FAQ / Customer',
+	'Class:lnkFAQToCustomer+' => 'A link between a FAQ and a Customer organization',
+	'Class:lnkFAQToCustomer/Attribute:faq_id' => 'FAQ',
+	'Class:lnkFAQToCustomer/Attribute:faq_id+' => '',
+	'Class:lnkFAQToCustomer/Attribute:faq_title' => 'FAQ',
+	'Class:lnkFAQToCustomer/Attribute:faq_title+' => '',
+	'Class:lnkFAQToCustomer/Attribute:customer_id' => 'Customer',
+	'Class:lnkFAQToCustomer/Attribute:customer_id+' => '',
+	'Class:lnkFAQToCustomer/Attribute:customer_name' => 'Customer',
+	'Class:lnkFAQToCustomer/Attribute:customer_name+' => '',
+));
---
+++ ru.dict.knowitop-faq-customer-access.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2018 Vladimir Kunin https://knowitop.ru
+ */
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:FAQ/Attribute:status' => 'Статус',
+	'Class:FAQ/Attribute:status+' => 'Опубликованные FAQ отображаются на клиентском портале',
+	'Class:FAQ/Attribute:status/Value:draft' => 'Черновик',
+	'Class:FAQ/Attribute:status/Value:draft+' => '',
+	'Class:FAQ/Attribute:status/Value:published' => 'Опубликованный',
+	'Class:FAQ/Attribute:status/Value:published+' => '',
+	'Class:FAQ/Attribute:status/Value:obsolete' => 'Устаревший',
+	'Class:FAQ/Attribute:status/Value:obsolete+' => '',
+	'Class:FAQ/Attribute:customers_list' => 'Заказчики',
+	'Class:FAQ/Attribute:customers_list+' => 'Данный FAQ доступен на портале песонам выбранных организаций',
+));
+
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+	'Class:lnkFAQToCustomer' => 'Связь FAQ/Заказчик',
+	'Class:lnkFAQToCustomer+' => 'Связь между FAQ и заказчиком',
+	'Class:lnkFAQToCustomer/Attribute:faq_id' => 'FAQ',
+	'Class:lnkFAQToCustomer/Attribute:faq_id+' => '',
+	'Class:lnkFAQToCustomer/Attribute:faq_title' => 'FAQ',
+	'Class:lnkFAQToCustomer/Attribute:faq_title+' => '',
+	'Class:lnkFAQToCustomer/Attribute:customer_id' => 'Заказчик',
+	'Class:lnkFAQToCustomer/Attribute:customer_id+' => '',
+	'Class:lnkFAQToCustomer/Attribute:customer_name' => 'Заказчик',
+	'Class:lnkFAQToCustomer/Attribute:customer_name+' => '',
+));
